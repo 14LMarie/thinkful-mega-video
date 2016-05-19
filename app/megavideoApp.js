3674f056e659bdf51ad47ae18aa6d10a675b118a
Lynsey Teague <lynsey.teague@yahoo.com>
2016-05-19 15:51:49 -0400

    double click and stop

--- app/megavideoApp.js
+++ app/megavideoApp.js
@@ -1,52 +1,57 @@
-angular.module('megaVideoDemo', [])
-    .directive('megaVideo', function ($sce) {
-        return {
-            restrict: 'E',
-            templateUrl: 'mega-video.html',
-            scope: true,
-            link: function (scope, element, attrs) {
-                scope.sources = [];
-                //whitelist of video formats accepted
-                function processSources() {
-                    var sourceTypes = {
-                        webm: {
-                            type: 'video/webm'
-                        },
-                        mp4: {
-                            type: 'video/mp4'
-                        },
-                        ogg: {
-                            type: 'video/ogg'
-                        }
-                    };
-                    for (source in sourceTypes) {
-                        if (attrs.hasOwnProperty(source)) {
-                            scope.sources.push({
-                                type: sourceTypes[source].type,
-                                src: $sce.trustAsResourceUrl(attrs[source])
-                            });
-                        }
+angular.module('megaVideoDemo', []).
+directive('megaVideo', function ($sce) {
+    return {
+        restrict: 'E',
+        templateUrl: 'mega-video.html',
+        scope: true,
+        link: function (scope, element, attrs) {
+            var videoPlayer = element.find('video')[0];
+            scope.sources = [];
+
+            function processSources() {
+                var sourceTypes = {
+                    webm: {
+                        type: 'video/webm'
+                    },
+                    mp4: {
+                        type: 'video/mp4'
+                    },
+                    ogg: {
+                        type: 'video/ogg'
                     }
                 }
-                processSources();
-                scope.video = {
-                    play: function () {
-                        videoPlayer.play();
-                        scope.video.status = 'play';
-                    },
-                    pause: function () {
-                        videoPlayer.pause();
-                        scope.video.status = 'pause';
-                    },
-                    stop: function () {
+                for (source in sourceTypes) {
+                    if (attrs.hasOwnProperty(source)) {
+                        scope.sources.push({
+                            type: sourceTypes[source].type,
+                            src: $sce.trustAsResourceUrl(attrs[source])
+                        });
+                    }
+                }
 
-                    },
-                    togglePlay: function () {
-                        scope.video.status == 'play' ? this.pause() : this.play();
-                    },
-                    width: attrs.width,
-                    height: attrs.height
-                };
             }
-        }
-    });
+            processSources();
+            scope.video = {
+                play: function () {
+                    videoPlayer.play();
+                    scope.video.status = 'play';
+                },
+                pause: function () {
+                    videoPlayer.pause();
+                    scope.video.status = 'pause';
+                },
+                stop: function () {
+                    videoPlayer.pause();
+                    videoPlayer.currentTime = 0;
+                    scope.video.status = 'stop'
+                },
+                togglePlay: function () {
+                    scope.video.status == 'play' ? this.pause() : this.play();
+                },
+                width: attrs.width,
+                height: attrs.height
+            };
+        },
+
+    }
+});
